2d7bd8c057a068999954e02ed15043f3501fd0fb
PatrykCodePy <patryk65249@gmail.com>
2021-11-26 21:26:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import foo
+import subprocess
+import os
 
-print(__name__)
-foo.print_foo(4)
+address = "8.8.8.8"
+res = subprocess.call(['ping', address])
+#os.system("ping  %s" % address)
+if res == 0:
+	print("ping to", address, "OK")
+elif res == 2:
+    print("no response from", address)
+else:
+    print("ping to", address, "failed!")
+
+#ping 8.8.8.8 TTL-114
